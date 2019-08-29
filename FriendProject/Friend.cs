2698f61665a811c08c7c68012b005a0b1a117915
max-student@outlook.com <nicholasbaker7010@gmail.com>
2019-08-29 13:44:45 -0400

    initial commit

---
+++ FriendProject/Friend.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FriendProject {
+    class Friend {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool BFF { get; set; }
+        public int HowLong { get; set; }
+
+
+
+
+
+    }
+}
